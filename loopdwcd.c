1cd725a25a69cbf65a36efca5a1c80266d6e5257
Vanshika Rawat <one.shika16@gmail.com>
2025-01-25 22:43:11 +0530

    Add files via upload

---
+++ loopdwcd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int num, count = 0;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    do{
+        if (num == 0) {
+           count = 1;
+           break;
+        } else {  
+           num/= 10; 
+           count++; 
+        }
+    }
+    while (num != 0);
+       
+    printf("Number of digits: %d\n", count);
+    printf("Vanshika Rawat 590013163\n");
+    return 0;
+}
